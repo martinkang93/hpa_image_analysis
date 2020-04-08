700105283099c9ab936de78f24a9914129c92b21
martinkang93 <martin.kang93@gmail.com>
2020-04-08 14:21:34 -0400

    created new notebooks for experiments

--- build_distance_matrix.py
+++ build_distance_matrix.py
@@ -11,82 +11,76 @@ import json
 
 img_vectorizer = InceptionV3Vectorizer()
 
-df = pd.read_csv('./data/colon_enhanced.csv')
-error_log = []
+organ = 'colon'
+
+if not os.path.exists('/data/hpa/{}/distance_matrices'.format(organ)):
+	os.makedirs('/data/hpa/{}/distance_matrices'.format(organ))
+if not os.path.exists('/data/hpa/{}/embeddings'.format(organ)):
+	os.makedirs('/data/hpa/{}/embeddings'.format(organ))
+
+df = pd.read_csv('./data/{}_enhanced.csv'.format(organ))
+
+if not os.path.exists('./error_log.pkl'):
+	error_log = []
+else:
+	error_log = pkl.load(open('./error_log.pkl'.format(organ), 'rb'))
 
 for gene in tqdm(df['gene'].unique()):
 	gene_df = df[df['gene']==gene]
 	for antibody in gene_df['antibody'].unique():
 
-		if os.path.exists('/data/hpa/colon/healthy_distance_matrices/{}.npy'.format(gene+'-'+antibody)): continue
-		if os.path.exists('/data/hpa/colon/cancer_distance_matrices/{}.npy'.format(gene+'-'+antibody)): continue
+		if os.path.exists('/data/hpa/{}/distance_matrices/{}.npy'.format(organ, gene+'-'+antibody)): continue
 
 		antibody_df = gene_df[gene_df['antibody']==antibody]
-		if len(antibody_df['disease_type'].unique()) == 2:
-			healthy = []
-			cancer = []
-			healthy_file = []
-			cancer_file = []
-			for index, row in antibody_df.iterrows():
-				filename = row['s3_aws_urls'].split('/')[-1].split('.')[0]
+		vector_list = []
+		file_list = []
+		for index, row in antibody_df.iterrows():
+			filename = row['s3_aws_urls'].split('/')[-1].split('.')[0]
 
-				if os.path.exists('/data/hpa/colon/embeddings/{}.npy'.format(filename)):
-					mean_vector = np.load('/data/hpa/colon/embeddings/{}.npy'.format(filename))
-				else:
-					img = img_load_aws(row['s3_aws_urls'])
-					if img is None: continue
-					if len(img.shape) != 3: continue
-					patch_list = patch_generator(img, (299, 299))
-					if not len(patch_list)>0:
-						error_log.append(row['s3_aws_urls'])
-						continue
-					features = img_vectorizer.generate_vectors(patch_list)
-					mean_vector = np.mean(features, axis=0)
-				
-				if row['disease_type'] != 'colorectal cancer':
-					healthy.append(mean_vector)
-					healthy_file.append(row['s3_aws_urls'])
-				else:
-					cancer.append(mean_vector)
-					cancer_file.append(row['s3_aws_urls'])
-					np.save('/data/hpa/colon/embeddings/{}.npy'.format(filename), mean_vector)
-
-				if not os.path.exists('/data/hpa/colon/embeddings/{}.npy'.format(filename)):
-					np.save('/data/hpa/colon/embeddings/{}.npy'.format(filename), mean_vector)
-
-			if (len(healthy) == 0) or (len(cancer) == 0):
+			if row['s3_aws_urls'] in error_log:
 				continue
 
-			#distance_matrix = scipy.spatial.distance.cdist(np.array(healthy), np.array(cancer), metric='cosine')
+			if os.path.exists('/data/hpa/{}/embeddings/{}.npy'.format(organ, filename)):
+				mean_vector = np.load('/data/hpa/{}/embeddings/{}.npy'.format(organ, filename))
+			else:
+				img = img_load_aws(row['s3_aws_urls'])
 
-			healthy_matrix = scipy.spatial.distance.cdist(np.array(healthy), np.array(healthy), metric='cosine')
-			cancer_matrix = scipy.spatial.distance.cdist(np.array(cancer), np.array(cancer), metric='cosine')
+				if (img is None) or (len(img.shape)!=3):
+					error_log.append(row['s3_aws_urls'])
+					pkl.dump(error_log, open('./error_log.pkl', 'wb'))						
+					continue
+
+				patch_list = patch_generator(img, (299, 299))
+
+				if not len(patch_list)>0:
+					error_log.append(row['s3_aws_urls'])
+					pkl.dump(error_log, open('error_log.pkl', 'wb'))
+					continue
+				
+				features = img_vectorizer.generate_vectors(patch_list)
+				mean_vector = np.mean(features, axis=0)
 			
-			#distance_pairs = []
+			vector_list.append(mean_vector)
+			file_list.append(row['s3_aws_urls'])
 
-			#for i in range(len(healthy_file)):
-			#	for j in range(len(cancer_file)):
-			#		distance_pairs.append([healthy_file[i], cancer_file[j], distance_matrix[i][j]])
+			if not os.path.exists('/data/hpa/{}/embeddings/{}.npy'.format(organ, filename)):
+				np.save('/data/hpa/{}/embeddings/{}.npy'.format(organ, filename), mean_vector)
 
-			healthy_pairs = []
-			cancer_pairs = []
+		if len(vector_list) == 0:
+			continue
 
-			for i in range(len(healthy_file)):
-				for j in range(len(healthy_file)):
-					healthy_pairs.append([healthy_file[i], healthy_file[j], healthy_matrix[i][j]])
+		distance_matrix = scipy.spatial.distance.cdist(np.array(vector_list), np.array(vector_list), metric='cosine')
+		
+		distance_pairs = []
 
-			for i in range(len(cancer_file)):
-				for j in range(len(cancer_file)):
-					cancer_pairs.append([cancer_file[i], cancer_file[j], cancer_matrix[i][j]])
-
+		for i in range(len(file_list)):
+			for j in range(len(file_list)):
+				distance_pairs.append([file_list[i], file_list[j], distance_matrix[i][j]])
 
 
-			#np.save('/data/hpa/colon/distance_matrices/{}.npy'.format(gene+'-'+antibody), distance_pairs)
-			np.save('/data/hpa/colon/healthy_distance_matrices/{}.npy'.format(gene+'-'+antibody), healthy_pairs)
-			np.save('/data/hpa/colon/cancer_distance_matrices/{}.npy'.format(gene+'-'+antibody), cancer_pairs)
 
-			# np.save('./data/distance_matrices/{}.npy'.format(gene+'-'+antibody), distance_matrix)
-		else:
-			print('No overlapping gene-antibody normal & cancer pairs')
+		np.save('/data/hpa/{}/distance_matrices/{}.npy'.format(organ, gene+'-'+antibody), distance_pairs)
+
+		# np.save('./data/distance_matrices/{}.npy'.format(gene+'-'+antibody), distance_matrix)
 
 
